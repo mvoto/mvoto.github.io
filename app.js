8fc868b92f0318e11f477ce1fd00489ddc737c16
Mauricio Voto <mauriciovoto@gmail.com>
2020-11-14 00:38:37 -0800

    add videos and updates template to use them

--- app.js
+++ app.js
@@ -4,53 +4,69 @@ const app = Vue.createApp({
       pageNumber: parseInt(localStorage.getItem('currentPage')) || 0,
       pagesData: [
         {
-          videoPath: './public/cappuccino.mp4',
-          title: 'Intro',
+          videoPath: './public/video-1.mp4',
+          title: '',
           question: 'Você conhece o Pinóquio?',
           answers: [],
           imageUrl: 'https://media1.tenor.com/images/45705162053cce4cf853d122b80a0899/tenor.gif?itemid=5227247',
           rightAnswer: null
         },
         {
-          videoPath: './public/Hpee77I.gif.mp4',
-          title: 'Pergunta 1',
-          question: 'Qual é a cor do cavalo branco de Napoleão?',
+          videoPath: './public/video-2.mp4',
+          title: '',
+          question: 'O que você gostaria de ganhar?',
           answers: [
-            'Branco',
-            'Preto',
-            'Azul',
-            'Marrom'
+            'Uma Boneca da Elsa',
+            'Um Carrinho HotWeels',
+            'Um PlayStation 5',
+            'Uma passagem de avião'
           ],
-          rightAnswer: 'Branco'
+          rightAnswer: null
         },
         {
-          videoPath: './public/Hpee77I.gif.mp4',
-          title: 'Pergunta 2',
-          question: 'Qual o nome do país que moramos?',
-          answers: [
-            'Brasil',
-            'EUA',
-            'Canadá',
-            'Itália'
-          ],
-          rightAnswer: 'Canadá'
+          videoPath: './public/video-3.mp4',
+          title: '',
+          question: 'Mentira não é legal...',
+          answers: [],
+          imageUrl: 'https://i.pinimg.com/originals/6b/b2/80/6bb280ebfbfe472c209efb4a2bc7b011.gif',
+          rightAnswer: null
         },
         {
-          videoPath: './public/cappuccino.mp4',
-          title: 'Pergunta 3',
-          question: 'Quantos anos tem o Mauricio?',
-          answers: [
-            '12',
-            '26',
-            '56',
-            '34'
-          ],
-          rightAnswer: '34'
+          videoPath: './public/video-4.mp4',
+          title: '',
+          question: 'O casal mentiroso',
+          answers: [],
+          imageUrl: 'https://media0.giphy.com/media/ftqLysT45BJMagKFuk/giphy.gif',
+          rightAnswer: null
         },
         {
-          videoPath: './public/cappuccino.mp4',
-          title: 'Encerramento',
-          question: 'Até a próxima!',
+          videoPath: './public/video-5.mp4',
+          title: '',
+          question: 'Mentirinhas mancham o coração',
+          answers: [],
+          imageUrl: 'https://i.pinimg.com/474x/ec/f1/e7/ecf1e7553873b711e24bd0a3406851db.jpg',
+          rightAnswer: null
+        },
+        {
+          videoPath: './public/video-7.mp4',
+          title: '',
+          question: null,
+          answers: [],
+          imageUrl: '',
+          rightAnswer: null
+        },
+        {
+          videoPath: './public/video-8.mp4',
+          title: '',
+          question: 'A bola virou melancia :)',
+          answers: [],
+          imageUrl: 'https://i.imgur.com/ORPMi1e.gif?noredirect',
+          rightAnswer: null
+        },
+        {
+          videoPath: './public/video-final-erro.mp4',
+          title: '',
+          question: 'Erro de gravação, kkkk. Até a próxima!',
           answers: [],
           imageUrl: 'https://i.gifer.com/5XEr.gif',
           rightAnswer: null
